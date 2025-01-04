9aa098062fbb7fd04f241c8fe974c4159f9c0bf7
n3vilreal <nebil.241423@ncit.edu.np>
2025-01-04 23:09:57 +0545

    Nebil

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 function Index() {
   return (
-    <div>
-        <h1>This is the landing page</h1>
-        <Link to="/buy">go donate</Link>
+    <div className='flex flex-col'>
+        <h1 className='text-8xl text-white bg-red-600 p-9 font-serif'>Fundraiser</h1>
+        <Link to="/buy" className='bg-[#28282b] text-white self-center my-5 px-5 py-1 rounded-lg'>Go donate</Link>
     </div>
   )
 }

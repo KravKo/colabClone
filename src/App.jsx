1c46208245689b5a5d493907bd02796b4d255929
KravKo <krishavkoirala99@gmail.com>
2025-01-05 17:06:31 +0545

    added a map directory

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/home/Index"
 import Buy from "./pages/buy/Index"
 import Success from "./pages/status/Index"
+import Map from "./pages/map/Index"
 import NotFound from "./pages/404/Index"
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/buy" element={<Buy />} />
           <Route path="/success" element={<Success />} />
+          <Route path="/map" element={<Map />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/map/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+function Index() {
+  return (
+    <>
+        <h1>This is map</h1>
+    </>
+  )
+}
+
+export default Index
